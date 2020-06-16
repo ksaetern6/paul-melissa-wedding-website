e014e5be20081975a1fc18e24cc12b608d0ea361
Ken Saetern <kensaetern@Kens-MacBook-Pro.local>
2020-06-15 23:27:48 -0700

    installed bootstrap and added public directory

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 app.listen(port, () => {
-    console.log('Server is up on port', port)
-})
+  console.log('Server is up on port', port);
+});
