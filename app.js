39d4dac7334e1751d56cb57ce848984f6b2448ce
Ken Saetern <kensaetern@Kens-MacBook-Pro.local>
2020-06-16 01:10:45 -0700

    added homepage template and css files

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Template Engine using Pug
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// Static file path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index', { title: 'Home', message: 'Hello World!' });
 });
 
 app.listen(port, () => {
-  console.log('Server is up on port', port);
+  // console.log('Server is up on port', port);
 });
